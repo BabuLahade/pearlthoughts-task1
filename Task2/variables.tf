778bb036bf7506e9ff08e59b26832261990d9c6a
BabuLahade <bbaulahade@gmail.com>
2026-02-03 18:24:11 +0530

    terraform Aws ec2

---
+++ Task2/variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+  default     = "eu-north-1a"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+  default     = "ami-0f5ee92e2d63afc18"
+}
+
+variable "key_name" {
+  description = "Key pair name"
+  type        = string
+  default     = "terraform-key"
+}
